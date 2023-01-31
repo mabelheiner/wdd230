ecb1bf6447cf15ffbac87740b2f1d0a25ff15516
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2023-01-30 23:38:18 -0700

    added dynamic navbar

--- chamber/scripts/header-footer.js
+++ chamber/scripts/header-footer.js
@@ -8,3 +8,11 @@ document.querySelector("#currentyear").textContent = today.getFullYear();
 
 // This sets last modified date on the home page
 document.querySelector('#lastmodified').textContent = document.lastModified;
+
+function toggleMenu(){
+    document.querySelector("#navbar").classList.toggle('menu-active')
+    document.querySelector("#menu-close").classList.toggle('menu-active')
+    document.querySelector("#menu-open").classList.toggle('menu-active')
+}
+
+document.querySelector('#hamburger-menu').addEventListener('click', toggleMenu)
