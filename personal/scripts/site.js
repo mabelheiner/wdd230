a0dfa0211e81b01eee5c500f13147aac1097b86d
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2023-02-01 14:36:00 -0700

    added personal library site

---
+++ personal/scripts/site.js
@@ -0,0 +1,18 @@
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full"}).format( new Date() );
+
+document.querySelector("#today").innerHTML = fulldate;
+
+// This sets the year for the footer
+const today = new Date();
+document.querySelector("#currentyear").textContent = today.getFullYear();
+
+// This sets last modified date on the home page
+document.querySelector('#lastmodified').textContent = document.lastModified;
+
+function toggleMenu(){
+    document.querySelector("#navbar").classList.toggle('menu-active')
+    document.querySelector("#menu-close").classList.toggle('menu-active')
+    document.querySelector("#menu-open").classList.toggle('menu-active')
+}
+
+document.querySelector('#hamburger-menu').addEventListener('click', toggleMenu)
