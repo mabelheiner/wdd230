3c1c7118e19af64a9915342533a39820ea6859d2
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2023-02-17 15:50:42 -0700

    added in discover page

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -3,3 +3,39 @@ var messagedate = new Date();
 if (messagedate.getDay()==1 || messagedate.getDay()==2) {
     document.querySelector("#meet-greet").classList.add("active");
 }
+
+//lazy loading
+// Gather the images to load
+let images = document.querySelectorAll("img[data-src]");
+
+// Set up the load images function which switches the src and the data-src attributes.
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+
+// Add an intersection observer 
+const callback = (items, observer) => {
+  items.forEach((item) => {
+    if (item.isIntersecting) {
+      loadImages(item.target);
+      observer.unobserve(item.target);
+    }
+  });
+};
+
+// Set up the options
+let options = {
+  threshold: 0.1,
+};
+
+// Create an observer
+const observer = new IntersectionObserver(callback, options);
+
+// Register each image with the intersection observer
+images.forEach((img) => {
+  observer.observe(img);
+});
