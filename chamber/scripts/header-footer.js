e5fffa344684f102054c2f18f4d2a645fda0066a
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2023-02-28 09:44:46 -0700

    added checks to the form

--- chamber/scripts/header-footer.js
+++ chamber/scripts/header-footer.js
@@ -1,6 +1,8 @@
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full"}).format( new Date() );
 
 document.querySelector("#today").innerHTML = fulldate;
+document.querySelector('#current_date').innerHTML = fulldate;
+document.querySelector('#current_date').value = fulldate;
 
 // This sets the year for the footer
 const today = new Date();
