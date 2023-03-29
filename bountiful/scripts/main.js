bf9a8b4c55196988911df3f0123accd9562b6a4a
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2023-03-28 19:15:02 -0600

    api prints out to console

--- bountiful/scripts/main.js
+++ bountiful/scripts/main.js
@@ -11,6 +11,27 @@ const apikey = '7c769c7df2eb7fc6070718ff416f4bfd';
 
 const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apikey}&units=imperial`;
 
+function displayDaily(data) {
+    var forecastEl = document.querySelector("forecast");
+    var fday = "";
+    console.log("Data,", data.list)
+    data.list.forEach((value, index) => {
+        if (index > 0) {
+            var dayname = new Date(value.dt * 1000).toLocaleDateString("en", {
+                weekday: "long",
+            });
+            var icon = value.weather[0].icon;
+            var temp = value.temp.day.toFixed(0);
+            fday = `<div class="forecast-day">
+                <p>${dayname}</p>
+                <p><span class="ico-${icon}" title="${icon}"></span></p>
+                <div class="forecast-day--temp">${temp}<sup>°C</sup></div>
+            </div>`;
+            forecastEl[0].insertAdjacentHTML('beforeend', fday);
+        }
+    });
+}
+
 async function apiFetch() {
     try {
       const response = await fetch(url);
@@ -18,12 +39,12 @@ async function apiFetch() {
         const data = await response.json();
         //console.log(data); // this is for testing the call
         console.log(data);
+        displayDaily(data);
       } else {
           throw Error(await response.text());
       }
     } catch (error) {
         console.log(error);
-        console.log(response);
     }
   }
   
