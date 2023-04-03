d16dad2892b9d71e1b2703539f00b27bb7991458
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2023-04-03 15:12:38 -0600

    corrected counter initialization

--- bountiful/scripts/orders.js
+++ bountiful/scripts/orders.js
@@ -3,19 +3,19 @@ let drink_count = document.querySelector('#drink-count');
 const order_button = document.querySelector(".order-button");
 
 let count = localStorage.getItem('drinks-counted');
+
+console.log("The initial count is", count);
+
+if (count === ""){
+    count = 0;
+}
+localStorage.setItem('drinks-counted', count);
 drink_count.innerHTML = localStorage.getItem('drinks-counted');
 
 console.log("drinks counted", localStorage.getItem('drinks-counted'));
 
 
 order_button.addEventListener('click', () => {
-
-    if (count === null){
-        count = 0;
-    }
-
-    else {
-        count++;
-    }
+    count++;
     localStorage.setItem('drinks-counted', count);
 })
