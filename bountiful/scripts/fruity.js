a2ea045b10ea221b9a7fd62b8508e432bf0074cf
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2023-04-03 18:15:05 -0600

    added selection options to form

--- bountiful/scripts/fruity.js
+++ bountiful/scripts/fruity.js
@@ -5,12 +5,42 @@ const fruity_url = "./data/fruity.json";
 const displayFruits = (fruits) => {
   const fruit_options = document.querySelector(".fruit-list");
 
+  // Get the select element
+  const select1 = document.getElementById("select1");
+  const select2 = document.getElementById("select2");
+  const select3 = document.getElementById("select3");
+
+  // Loop through the options and create option elements
+  for (let i = 0; i < fruits.length; i++) {
+    const option = document.createElement("option");
+    option.value = fruits[i].id;
+    option.text = fruits[i].name;
+    select1.add(option);
+  }
+
+  for (let i = 0; i < fruits.length; i++) {
+    const option = document.createElement("option");
+    option.value = fruits[i].id;
+    option.text = fruits[i].name;
+    select2.add(option);
+  }
+
+  for (let i = 0; i < fruits.length; i++) {
+    const option = document.createElement("option");
+    option.value = fruits[i].id;
+    option.text = fruits[i].name;
+    select3.add(option);
+  }
+
+
+  /*
   let num = 0;
   fruits.forEach((fruit) => {
     fruit_options.innerHTML += `<div class="item"><input type="checkbox" id=fruit name=fruit value=${fruit.name}>
     <label for=${fruit.id}> ${fruit.name}</label></div>`;
     num += 1;
   })
+  */
 }; 
 
 async function getFruitData() {
